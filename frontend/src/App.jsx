31895750e1b8a61c39e7049dc8e7e8249d172116
NitroRonak <ronaks0988@gmail.com>
2024-02-17 22:26:27 +0530

    basic setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-
 function App() {
-  
   return (
     <>
-      Hello
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
