38cc120fa67336d1eb57e0cb6f1ecd879a86f13a
NitroRonak <ronaks0988@gmail.com>
2024-02-18 01:03:25 +0530

    login and logout controller and routes done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,3 +33,38 @@ export const createUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid user data");
   }
 });
+
+
+export const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+  
+    const existingUser = await User.findOne({ email });
+  
+    if (existingUser) {
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+  
+      if (isPasswordValid) {
+        createToken(res, existingUser._id);
+  
+        res.status(201).json({
+          _id: existingUser._id,
+          username: existingUser.username,
+          email: existingUser.email,
+          isAdmin: existingUser.isAdmin,
+        });
+        return;
+      }
+    }
+  });
+
+  export const logoutCurrentUser = asyncHandler(async (req, res) => {
+    res.cookie("jwt", "", {
+      httyOnly: true,
+      expires: new Date(0),
+    });
+  
+    res.status(200).json({ message: "Logged out successfully" });
+  });
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, loginUser, logoutCurrentUser } from "../controllers/userController.js";
 const router = express.Router();
 
 router.route("/").post(createUser);
+router.route("/auth").post(loginUser);
+router.route("/logout").post(logoutCurrentUser);
 
 export default router;
