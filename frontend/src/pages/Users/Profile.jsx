615971a1835181767cb23941f66f7511238f52a8
NitroRonak <ronaks0988@gmail.com>
2024-02-19 22:24:25 +0530

    Profile Page Done.

---
+++ frontend/src/pages/Users/Profile.jsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
+import { useProfileMutation } from "../../redux/api/usersApiSlice";
+import Loader from "../../components/Loader";
+import { setCredientials } from "../../redux/features/auth/authSlice";
+import { Link } from "react-router-dom";
+
+const Profile = () => {
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [updateProfile, { isLoading: loadingUpdateProfile }] =
+    useProfileMutation();
+
+  useEffect(() => {
+    setUserName(userInfo.username);
+    setEmail(userInfo.email);
+  }, [userInfo.email, userInfo.username]);
+
+  const dispatch = useDispatch();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match");
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          username,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredientials({ ...res }));
+        toast.success("Profile updated successfully");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4 mt-[5rem] h-fit">
+      <div className="flex justify-center align-center md:flex md:space-x-4">
+        <div className="md:w-1/2 w-2/4 flex flex-col gap-2">
+          <h2 className="text-3xl font-semibold mb-4">Update Profile</h2>
+          <form onSubmit={submitHandler}>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Name</label>
+              <input
+                type="text"
+                placeholder="Enter name"
+                className="form-input p-4 rounded-sm w-full outline-none"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-white mb-2">Email Address</label>
+              <input
+                type="email"
+                placeholder="Enter email"
+                className="form-input p-4 rounded-sm w-full outline-none"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-white mb-2">Password</label>
+              <input
+                type="password"
+                placeholder="Enter password"
+                className="form-input p-4 rounded-sm w-full outline-none"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-white mb-2">Confirm Password</label>
+              <input
+                type="password"
+                placeholder="Confirm password"
+                className="form-input p-4 rounded-sm w-full outline-none"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="flex justify-between">
+              <button
+                type="submit"
+                className="bg-purple-500 text-white py-2 px-4 rounded hover:bg-purple-800"
+              >
+                Update
+              </button>
+
+              <Link
+                to="/user-orders"
+                className="bg-purple-600 text-white py-2 px-4 rounded hover:bg-purple-800"
+              >
+                My Orders
+              </Link>
+            </div>
+            {loadingUpdateProfile && <Loader />}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
