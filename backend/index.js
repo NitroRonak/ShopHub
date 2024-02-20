25ad23a01faa8d656461c27e140b41b912ed1176
NitroRonak <ronaks0988@gmail.com>
2024-02-20 13:39:33 +0530

    Category Model,Controller,Routes Done

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js"
+import categoryRoutes from "./routes/categoryRoutes.js";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use("/api/users",userRoutes)
+app.use("/api/users",userRoutes);
+app.use("/api/category",categoryRoutes);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
